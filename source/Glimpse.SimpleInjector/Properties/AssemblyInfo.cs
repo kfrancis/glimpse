f4eb07ed9a93cdbdabc22cf68bae7e2101fcee52
NET Junkie <steven@cuttingedge.it>
2017-03-31 09:11:05 +0200

    Upgraded Glimpse plugin to Simple Injector v4.

--- source/Glimpse.SimpleInjector/Properties/AssemblyInfo.cs
+++ source/Glimpse.SimpleInjector/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Simple Injector Contributors")]
 [assembly: AssemblyProduct("Glimpse.SimpleInjector")]
-[assembly: AssemblyCopyright("Copyright © 2013-2015 Simple Injector Contributors")]
+[assembly: AssemblyCopyright("Copyright © 2013-2017 Simple Injector Contributors")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
